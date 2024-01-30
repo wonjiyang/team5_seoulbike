7629ff87d2a6bb9adcc0ea594f30b441cabc5934
study-MY <null.for.ez@gmail.com>
2024-01-30 16:24:22 +0900

    상품안내 반응형 토글콘텐츠

--- assets/js/sub_ticket_info.js
+++ assets/js/sub_ticket_info.js
@@ -1,23 +1,17 @@
 // 만든거 일단 가져옴
 
-$(document).ready(function () {
-  // Toggle button click event
-  $("#toggleButton").on("click", function () {
-    // Toggle the 'hidden' class on the content div
-    $("#content").toggleClass("hidden");
-  });
-
+$(function () {
   // 토큰 콘텐츠
   function toggleContent() {
     var viewportWidth = window.innerWidth;
 
     // Check the viewport width and toggle classes accordingly
-    if (viewportWidth <= 600) {
-      $("#tableContent").addClass("hidden");
-      $("#listContent").removeClass("hidden");
+    if (viewportWidth >= 1024) {
+      $(".tic-for-m").addClass("hidden");
+      $(".tic-for-d").removeClass("hidden");
     } else {
-      $("#tableContent").removeClass("hidden");
-      $("#listContent").addClass("hidden");
+      $(".tic-for-m").removeClass("hidden");
+      $(".tic-for-d").addClass("hidden");
     }
   }
 
