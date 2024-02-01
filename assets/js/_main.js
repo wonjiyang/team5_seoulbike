b6dcdf3d50d0f9c08bf0732504754650dca7690a
wonjiyang <wonjiyang@gmail.com>
2024-02-01 15:48:30 +0900

    map 추가중

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -33,11 +33,15 @@ function initMap() {
         zoom: 16
     });
 
+    // 공통 스타일 설정
+    var contentStyle1 = 'position: relative; width: 180px; height: 32px;  border-radius: 15px; font-size: 14px; text-align: center; background-color: #3D5567; color: #FFF;';
+    var contentStyle2 = 'height: 80px; border-radius: 15px;';
+
     // 마커 정보 정의
     var markersInfo = [
-        { position: new naver.maps.LatLng(37.504678975306454, 127.02507115055475), title: "신논현역 4번 출구", content: "정보 내용", number: "23" },
-        { position: new naver.maps.LatLng(37.503283707385826, 127.02476794307636), title: "신논현역 7번 출구", content: "정보 내용", number: "28" },
-        { position: new naver.maps.LatLng(37.50417055795581, 127.0238787848778), title: "신논현역 9번 출구", content: "정보 내용", number: "11" },
+        { position: new naver.maps.LatLng(37.504678975306454, 127.02507115055475), title: "신논현역 4번 출구", content: '<div style="' + contentStyle1 + '">신논현역 4번 출구</div><div style="' + contentStyle2 + '"><p>내용1</p><p>내용2</p></div>', number: "23" },
+        { position: new naver.maps.LatLng(37.503283707385826, 127.02476794307636), title: "신논현역 7번 출구", content: '<div style="' + contentStyle1 + '">신논현역 7번 출구</div><div style="' + contentStyle2 + '"><p>내용1</p><p>내용2</p></div>', number: "28" },
+        { position: new naver.maps.LatLng(37.50417055795581, 127.0238787848778), title: "신논현역 9번 출구", content: '<div style="' + contentStyle1 + '">신논현역 9번 출구</div><div style="' + contentStyle2 + '"><p>내용1</p><p>내용2</p></div>', number: "11" },
     ];
 
     var infoWindow;
@@ -49,23 +53,7 @@ function initMap() {
             map: map,
             title: markerInfo.title,
             icon: {
-                content: 
-                '<div style="position: relative; width: 32px; height: 32px; text-align: center;">  <img src="./images/main/marker.png" alt="" style="width: 100%; height: auto;"><span style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); color: $subcolor; font-size: 15px; font-weight: 700;">' + markerInfo.number + '</span></div>',
-                size: new naver.maps.Size(32, 32),
-                anchor: new naver.maps.Point(16, 32)
-            }
-        });
-
-        var marker = new naver.maps.Marker({
-            position: markerInfo.position,
-            map: map,
-            title: markerInfo.title,
-            icon: {
-                content: 
-                '<div class="marker-content">' + 
-                '<img src="./images/main/marker.png" alt="" style="width: 100%; height: auto;">' +
-                '<span>' + markerInfo.number + '</span>' +
-                '</div>',
+                content: '<img src="./images/main/marker.png" alt="" style="width: 100%; height: auto;"><span style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); color: $subcolor; font-size: 15px; font-weight: 700;">' + markerInfo.number + '</span></div>',
                 size: new naver.maps.Size(32, 32),
                 anchor: new naver.maps.Point(16, 32)
             }
@@ -77,10 +65,12 @@ function initMap() {
                 infoWindow.close();
             }
             infoWindow = new naver.maps.InfoWindow({
-                content: markerInfo.content
+                content: markerInfo.content,
+                borderColor: '#000', // 테두리 색상
+                borderRadius: 15 // 테두리 반지름
             });
             infoWindow.open(map, marker);
-        });                  
+        });                      
     });
 
     // 지도 클릭 시 정보 창 닫기
@@ -90,4 +80,3 @@ function initMap() {
         }
     });
 }
-
