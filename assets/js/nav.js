377ea4f58e536428f7f336323b691de0eebc97df
study-MY <null.for.ez@gmail.com>
2024-02-16 19:14:55 +0900

    m_nav ing

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,18 +1,32 @@
 $(
   // menu open?
-  $(".btn_menu").click(function () {
-    // menu를 열어야 함.
+  $(".btn_menu").click(
+    function () {
+      var o = $(this).attr("href"),
+        o = o.split("#")[1];
+      $body.removeClass("srch-open");
+      $body.hasClass("nav-open") ? lyrClose(o) : lyrOpen(o);
+      return false;
+    },
+    function (t) {
+      return "undefined" != typeof w && w.event.triggered !== t.type
+        ? w.event.dispatch.apply(e, arguments)
+        : void 0;
+    }
+    //     function () {
+    //     // menu를 열어야 함.
 
-    // $('body').stop().toggleClass('open');
-    // $('#header').stop().toggleClass('open');
-    $("m_depth1").stop().toggleClass("open");
-    // $('.h_b2b').stop().toggleClass('open');
-    // $('.h_language').stop().toggleClass('open');
-    // $('#container').stop().toggleClass('blur');
-    // $('#footer').stop().toggleClass('blur');
-    // $('.h_category').stop().toggleClass('blur');
-    // $('.hm_partner').stop().toggleClass('blur');
-  }),
+    //     // $('body').stop().toggleClass('open');
+    //     // $('#header').stop().toggleClass('open');
+    //     $("m_depth1").stop().toggleClass("open");
+    //     // $('.h_b2b').stop().toggleClass('open');
+    //     // $('.h_language').stop().toggleClass('open');
+    //     // $('#container').stop().toggleClass('blur');
+    //     // $('#footer').stop().toggleClass('blur');
+    //     // $('.h_category').stop().toggleClass('blur');
+    //     // $('.hm_partner').stop().toggleClass('blur');
+    //   }
+  ),
 
   //depth 1 디테일 온오프
   $(".m_depth1 > li > strong").click(function () {
