1313c3677d1c46db17a6d419a5d96ddaffb258ad
study-MY <null.for.ez@gmail.com>
2024-02-16 19:24:08 +0900

    nav script try and error

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -29,18 +29,49 @@ $(
   ),
 
   //depth 1 디테일 온오프
-  $(".m_depth1 > li > strong").click(function () {
-    if ($(this).hasClass("active") === true) {
-      if ($(this).next().length > 0) {
-        // 자식이 하나 이상 있으면
-        $(this).next().removeClass("active");
+  // $(".m_depth1 > li > strong").click(function () {
+  //   if ($(this).hasClass("active") === true) {
+  //     if ($(this).next().length > 0) {
+  //       // 자식이 하나 이상 있으면
+  //       $(this).next().removeClass("active");
+  //     }
+  //     $(this).removeClass("active");
+  //   } else {
+  //     if ($(this).next().length > 0) {
+  //       $(this).next().addClass("active");
+  //     }
+  //     $(this).addClass("active");
+  //   }
+  // })
+
+  // 모든 .m_depth2를 숨김
+  $(".m_depth2").hide(),
+
+  // strong 태그를 클릭했을 때의 동작
+  $(".m_depth1 > li > strong").click(
+    function () {
+      if ($(this).hasClass("active") === true) {
+        if ($(this).next().length > 0) {
+          // 자식이 하나 이상 있으면
+          $(this).next().removeClass("active");
+        }
+        $(this).removeClass("active");
+      } else {
+        if ($(this).next().length > 0) {
+          $(this).next().addClass("active");
+        }
+        $(this).addClass("active");
       }
-      $(this).removeClass("active");
-    } else {
-      if ($(this).next().length > 0) {
-        $(this).next().addClass("active");
-      }
-      $(this).addClass("active");
+    },
+    function () {
+      // 현재 클릭한 strong 태그의 부모 요소인 li 태그의 다음 형제 요소인 ul 태그를 선택
+      var submenu = $(this).parent().find(".m_depth2");
+
+      // 다른 .m_depth2를 숨김
+      $(".m_depth2").not(submenu).hide();
+
+      // 현재 클릭한 strong 태그의 부모 요소인 li 태그에서 해당 .m_depth2를 토글하여 보여주거나 숨김
+      submenu.slideToggle();
     }
-  })
+  )
 );
